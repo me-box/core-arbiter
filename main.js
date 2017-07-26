f069a63527739537ed124d7e4b3cb0ff65a2b4e8
Yousef Amar <yousefamar@gmail.com>
2017-07-26 16:00:55 +0000

    Undo breaking bugfix and honour suggested secret length

--- main.js
+++ main.js
@@ -358,12 +358,13 @@ app.get('/store/secret', function (req, res) {
 	}
 
 	if (req.container.secret) {
-		res.send(req.container.secret);
+        	res.send(new Buffer(req.container.secret).toString('base64'));
 		return;
 	}
 
-        req.container.secret = new Buffer(randomstring.generate({ length: 128 })).toString('base64');
-        res.send(req.container.secret);
+	// NOTE: Actually should be 8 / log2(62), but 4 / 3 is close enough.
+        req.container.secret = randomstring.generate({ length: Math.ceil((4 / 3) * macaroons.MacaroonsConstants.MACAROON_SUGGESTED_SECRET_LENGTH) });
+        res.send(new Buffer(req.container.secret).toString('base64'));
 
 });
 
